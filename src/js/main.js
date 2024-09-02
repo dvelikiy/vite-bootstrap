ed385f63e7535221e8fda4e57492102aa8ae6d23
dvelikiy <dmitry.velikij@gmail.com>
2024-09-02 21:43:04 +0300

    fun

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import confetti from 'canvas-confetti';
 // Import our custom CSS
 import '../scss/styles.scss'
 
@@ -35,3 +36,17 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+const button = document.querySelector('.claim-button');
+button.addEventListener('click', () => {
+  function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+  confetti({
+    angle: randomInRange(55, 125),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(50, 100),
+    origin: { y: 0.6 }
+  });
+})
